0e764e8f08afc8c94dbfd0b12965097e36eca6c9
adham90 <hi@adham.dev>
2025-11-25 19:16:40 +0200

    improve design

--- app/controllers/ruby_llm/agents/executions_controller.rb
+++ app/controllers/ruby_llm/agents/executions_controller.rb
@@ -4,9 +4,9 @@ module RubyLLM
   module Agents
     class ExecutionsController < ApplicationController
       def index
-        @executions = filtered_executions.recent(50)
         @agent_types = Execution.distinct.pluck(:agent_type)
         @statuses = Execution.statuses.keys
+        load_paginated_executions
 
         respond_to do |format|
           format.html
@@ -19,20 +19,40 @@ module RubyLLM
       end
 
       def search
-        @executions = filtered_executions.recent(50)
+        @agent_types = Execution.distinct.pluck(:agent_type)
+        @statuses = Execution.statuses.keys
+        load_paginated_executions
 
         respond_to do |format|
+          format.html { render :index }
           format.turbo_stream do
             render turbo_stream: turbo_stream.replace(
               "executions_list",
               partial: "ruby_llm/agents/executions/list",
-              locals: { executions: @executions }
+              locals: { executions: @executions, pagination: @pagination }
             )
           end
         end
       end
 
       private
+
+      def load_paginated_executions
+        page = (params[:page] || 1).to_i
+        per_page = 25
+        offset = (page - 1) * per_page
+
+        base_scope = filtered_executions.order(created_at: :desc)
+        total_count = base_scope.count
+        @executions = base_scope.limit(per_page).offset(offset)
+
+        @pagination = {
+          current_page: page,
+          per_page: per_page,
+          total_count: total_count,
+          total_pages: (total_count.to_f / per_page).ceil
+        }
+      end
 
       def filtered_executions
         scope = Execution.all
